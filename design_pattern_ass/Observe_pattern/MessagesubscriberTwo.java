d6688914e24c2715d418f213355d785478cc83b5
Tahsinoshin <48786946+Tahsinoshin@users.noreply.github.com>
2022-03-31 08:52:38 +0000

    Update MessagesubscriberTwo.java

--- design_pattern_ass/Observe_pattern/MessagesubscriberTwo.java
+++ design_pattern_ass/Observe_pattern/MessagesubscriberTwo.java
@@ -1,1 +1,9 @@
 
+package Observe_pattern;
+
+public class MessageSubscriberTwo implements Observer {
+    @Override
+    public void update(Message m) {
+        System.out.println("MessageSubscriberTwo :: " + m.getMessageContent());
+    }
+}
